5a72f65489bf74c4c5e912926fef2680fff8eba2
Yosssef9 <147534893+Yosssef9@users.noreply.github.com>
2026-05-21 09:58:03 +0300

    Fix Strapi production admin dashboard and deployment config

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -2,21 +2,28 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
+
   {
     name: "strapi::cors",
     config: {
       origin: [
+        "https://qnhospital.com.sa",
+        "https://www.qnhospital.com.sa",
+
         "http://localhost:4174",
         "http://127.0.0.1:4174",
         "http://10.0.110.28:4174",
         "http://localhost:5173",
-        "https://h6cntfzw-5173.euw.devtunnels.ms",
       ],
+
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+
       credentials: true,
     },
   },
+
   "strapi::poweredBy",
   "strapi::query",
   "global::upload-rate-limit",
